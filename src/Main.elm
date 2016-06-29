6e8495bca14000e04becea1fd9b83b921a96bcd2
Kim Maida <git@kim-maida.com>
2016-06-28 20:56:44 -0400

    * Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,85 @@
+port module Main exposing (..)
+
+import Html exposing (..)
+import Html.App as Html
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+
+import Auth0
+import Authentication
+
+main : Program Never
+main = 
+    Html.program 
+        { init = init 
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
+    
+{- 
+    MODEL
+    * Model type 
+    * Initialize model with empty values
+    * Initialize with a random quote
+-}
+
+type alias Model =
+    { authModel : Authentication.Model
+    }
+    
+init : ( Model, Cmd Msg)  
+init =
+    ( Model (Authentication.init auth0showLock), Cmd.none )
+       
+{-
+    UPDATE
+    * Messages
+    * Update case
+-}
+
+-- Messages
+
+type Msg 
+    = Authentication Authentication.Msg 
+
+-- Ports
+
+port auth0showLock : Auth0.Options -> Cmd msg
+port auth0authResult : (Auth0.RawAuthenticationResult -> msg) -> Sub msg
+-- port setStorage : Model -> Cmd msg  
+-- port removeStorage : Model -> Cmd msg        
+
+-- Update
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Authentication authMsg ->
+            let
+                ( authModel, cmd ) = Authentication.update authMsg model.authModel
+            in
+                ( { model | authModel = authModel }, Cmd.map Authentication cmd )
+                       
+{-
+    VIEW
+    * Get a quote
+-}
+
+view : Model -> Html Msg
+view model =
+    div [ class "row" ] [
+        div [ class "jumbotron col-md-offset-4 col-md-6 text-center" ]
+            [ div []
+                ( case Authentication.tryGetUserProfile model.authModel of
+                    Nothing -> [ text "Please log in" ]
+                    Just user ->
+                        [ img [ height 50, width 50, src user.picture ] []
+                        , text ("Welcome, " ++ user.given_name ++ ".")
+                        ]
+                )
+            , button
+                [ class "btn btn-primary", onClick (Authentication (if Authentication.isLoggedIn model.authModel then Authentication.LogOut else Authentication.ShowLogIn)) ]
+                [ text (if Authentication.isLoggedIn model.authModel then "Logout" else "Login")]
+            ]
+    ]    
